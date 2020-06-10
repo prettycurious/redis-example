5c8b68b78647823341869f1887d6d5844e831fcc
Winsen.Tian <winsen.tian@gmail.com>
2020-06-10 21:36:09 +0800

    init commit

---
+++ src/main/java/com/example/redis/RedisUtil.java
@@ -0,0 +1,11 @@
+package com.example.redis;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author tianwenyuan
+ * Date: 2020/6/8
+ * Time: 10:50 上午
+ */
+public class RedisUtil {
+}
---
+++ src/main/java/com/example/redis/controller/RedisController.java
@@ -0,0 +1,16 @@
+package com.example.redis.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author tianwenyuan
+ * Date: 2020/6/8
+ * Time: 10:58 上午
+ */
+@Controller
+@RequestMapping("/redis")
+public class RedisController {
+}
